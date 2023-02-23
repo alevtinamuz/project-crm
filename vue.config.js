edf710f13a13585261bdb88bb931ca4a5639cf7d
alevtinamuz <113981269+alevtinamuz@users.noreply.github.com>
2023-02-23 13:25:24 +1000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false
-}),
+})
 module.exports = {
   project-crm: process.env.NODE_ENV === 'production'
     ? '/my-project/'
